ffa2dac0c1c03552b08fbb49f5574783a668d245
Matthieu Auger <matthieua@theodo.fr>
2020-05-10 14:07:01 +0200

    Paginate

--- functions/generate-calendar/generate-calendar.js
+++ functions/generate-calendar/generate-calendar.js
@@ -2,55 +2,42 @@ const SpotifyWebApi = require("spotify-web-api-node");
 const ical = require("ical-generator");
 const moment = require("moment");
 
-async function getRecentlyPlayedTracks(spotifyApi, tracksOffet) {
-  const recentlyPlayedTracksRequest = await spotifyApi.getMyRecentlyPlayedTracks(
-    {
-      limit: 50,
-      offset: tracksOffet,
-    }
-  );
-
-  return recentlyPlayedTracksRequest.body;
-}
-
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const cal = ical({
-      domain: "https://spotify-time-machine.netlify.app",
-      name: "Spotify Time Machine",
-    });
+    const cal = generateCalendar();
+    const spotifyApi = buildSpotifyApi();
 
-    const spotifyApi = new SpotifyWebApi();
-    spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
+    // for now we stop the calendar generation to 1 month ago
+    const lastDateInThePastToDisplay = moment().subtract(1, "month"); // 03-05
+    let dateToFetchBefore = moment(); // 10-05
 
-    let tracksOffet = 0;
     do {
+      // fetch last 50 played song before now
       const recentlyPlayedTracks = await getRecentlyPlayedTracks(
         spotifyApi,
-        tracksOffet
+        dateToFetchBefore
       );
-      recentlyPlayedTracks.items.map(function (track) {
-        const trackNameAlbumArtist = `Track: ${track.track.name}, Album: ${
-          track.track.album.name
-        }, Artist: ${track.track.artists
-          .map(function (artist) {
-            return artist.name;
-          })
-          .join(", ")}`;
 
-        return cal.createEvent({
-          start: moment(track.played_at),
-          end: moment(track.played_at).add(
-            track.track.duration_ms,
-            "millisecond"
-          ),
+      if (recentlyPlayedTracks.items.length === 0) {
+        break;
+      }
+
+      for (let i = 0; i < recentlyPlayedTracks.items.length; ++i) {
+        const track = recentlyPlayedTracks.items[i];
+        const trackNameAlbumArtist = generateEventDescription(track);
+        const trackPlayedAt = moment(track.played_at);
+        // for next loop, we will fetch song before the oldest one of this loop
+        dateToFetchBefore = trackPlayedAt.clone();
+
+        cal.createEvent({
+          start: trackPlayedAt,
+          end: trackPlayedAt.add(track.track.duration_ms, "millisecond"),
           summary: trackNameAlbumArtist,
           description: track.track.external_urls.spotify,
         });
-      });
-      tracksOffet += 50;
-    } while (tracksOffet < 500); // we fetch 500 tracks in the past
+      }
+    } while (dateToFetchBefore > lastDateInThePastToDisplay);
 
     return {
       statusCode: 200,
@@ -60,3 +47,39 @@ exports.handler = async (event, context) => {
     return { statusCode: 500, body: err.toString() };
   }
 };
+
+async function getRecentlyPlayedTracks(spotifyApi, before) {
+  console.log(`Calling with ${before}`);
+  const recentlyPlayedTracksRequest = await spotifyApi.getMyRecentlyPlayedTracks(
+    {
+      limit: 50,
+      before: before.unix() * 1000,
+    }
+  );
+
+  return recentlyPlayedTracksRequest.body;
+}
+
+function generateEventDescription(track) {
+  return `Track: ${track.track.name}, Album: ${
+    track.track.album.name
+  }, Artist: ${track.track.artists
+    .map(function (artist) {
+      return artist.name;
+    })
+    .join(", ")}`;
+}
+
+function generateCalendar() {
+  return ical({
+    domain: "https://spotify-time-machine.netlify.app",
+    name: "Spotify Time Machine",
+  });
+}
+
+function buildSpotifyApi() {
+  const spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
+
+  return spotifyApi;
+}
